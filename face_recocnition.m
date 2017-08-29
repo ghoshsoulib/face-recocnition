03f94370e791384d605739838a7f2ee4deb61b55
Soulib Ghosh <31388426+ghoshsoulib@users.noreply.github.com>
2017-08-29 13:26:49 +0530

    Add files via upload

---
+++ face_recocnition.m
@@ -0,0 +1,31 @@
+% taking the image input
+
+img = imread('E:\face_detection\sample image\9.PNG');
+figure,imshow(img), title('original image');
+
+% get face detector object
+%basically the algorithm for detecting face which is a inbuild function
+
+%FaceDetector = vision.CascadeObjectDetector();
+
+%FaceDetector = vision.CascadeObjectDetector('Mouth');
+
+FaceDetector = vision.CascadeObjectDetector('EyePairSmall');
+
+% FaceDetector.MergeThreshold = 90;
+
+% using face detector on the given image
+
+BBOX = step(FaceDetector, img);
+
+% Annotate these faces on the top of the image
+
+%B = insertObjectAnnotation(img, 'rectangle', BBOX, 'face');
+
+%B = insertObjectAnnotation(img, 'rectangle', BBOX, 'Mouth');
+
+B = insertObjectAnnotation(img, 'rectangle', BBOX, 'Eye');
+
+figure, imshow(B), title('face detected image');
+
+
